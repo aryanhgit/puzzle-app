e48b45d42958210711d53045d90bebf07e21f57c
ardawgcl3 <150267246+aryanhgit@users.noreply.github.com>
2025-08-01 21:03:17 +0530

    Added wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -27,3 +27,6 @@ api.add_namespace(auth_ns)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Posts': Posts, 'User': User}
+
+if __name__ == "__main__":
+    app.run(debug=True)
